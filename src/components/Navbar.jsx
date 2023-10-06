1fda1c270bd3c0237521486716b3c776ee0d3d15
samuel <olaoluwasamuel119@gmail.com>
2023-10-06 09:02:36 -0600

    new edit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
   const handleClick = () => setNav(!nav);
 
   return (
-    <div className="fixed w-screen h-[70px] top-0 z-50 flex justify-between items-center px-4 bg-green-400 text-black">
+    <div className="fixed w-screen h-[70px] top-0 z-50 flex justify-between items-center px-4 bg-green-400 text-white">
       <div className="text-1xl font-semibold ml-[20px ]">SolarInc</div>
 
       {/* menu */}
@@ -82,10 +82,10 @@ const Navbar = () => {
         className={
           !nav
             ? 'hidden'
-            : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
+            : 'absolute top-0 left-0 text-sm  w-full h-screen text-white bg-green-400 flex flex-col justify-center items-center'
         }
       >
-        <li className="py-6 text-4xl">
+        <li className="py-6 text-2xl ">
           <Link
             onClick={handleClick}
             to="home"
@@ -95,7 +95,7 @@ const Navbar = () => {
             Home
           </Link>
         </li>
-        <li className="py-6 text-4xl">
+        <li className="py-6 text-2xl">
           {' '}
           <Link
             onClick={handleClick}
@@ -106,7 +106,7 @@ const Navbar = () => {
             About
           </Link>
         </li>
-        <li className="py-6 text-4xl">
+        <li className="py-6 text-2xl">
           {' '}
           <Link
             onClick={handleClick}
@@ -117,7 +117,7 @@ const Navbar = () => {
             Service
           </Link>
         </li>
-        <li className="py-6 text-4xl">
+        <li className="py-6 text-2xl">
           {' '}
           <Link
             onClick={handleClick}
@@ -128,7 +128,7 @@ const Navbar = () => {
             Work
           </Link>
         </li>
-        <li className="py-6 text-4xl">
+        <li className="py-6 text-2xl">
           {' '}
           <Link
             onClick={handleClick}
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,22 +1,25 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 const Login = () => {
-  const [email, setEmail] = useState('micheal@gmail.com');
-  const [password, setPassword] = useState('badman01');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [error, setError] = useState('');
   const history = useNavigate();
   function handleSubmit(e) {
     e.preventDefault();
-    if (email === 'micheal@gmail.com' && password === 'baldman01') {
+    if (email === 'Influxsolarinc@gmail.com' && password === 'baldman01') {
       // Successful login, navigate to the home page
       history('/home');
+      setEmail('');
+      setPassword('');
+      setError('');
     } else {
       setError('Incorrect email or password');
     }
   }
 
   const bgImageStyle = {
-    backgroundImage: `url('https://images.unsplash.com/photo-1509391366360-2e959784a276?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80')`,
+    backgroundImage: `url('https://res.cloudinary.com/dutvrzezk/image/upload/v1696377012/pexels-ki-media-9799757_tgiwyc.jpg')`,
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
     height: '100vh',
@@ -27,31 +30,32 @@ const Login = () => {
       className=" flex items-center justify-center"
     >
       <div className=" max-w-[300px] w-[100%] mx-auto items-center justify-center flex flex-col ">
-        <div className="  bg-white pt-[30px] pb-[60px]">
+        <div className="  bg-white flex gap-5 flex-col items-center justify-center  lg:p-20">
           <h1 className="text-center text-[30px] font-semibold mb-6">LOGIN</h1>
 
-          <form onSubmit={handleSubmit}>
-            <div>
+          <form
+            onSubmit={handleSubmit}
+            className="flex flex-col items-center gap-7"
+          >
+            <div className="flex flex-col items-center justify-center gap-1">
               <input
-                className="mb-4 border-2 flex justify-center items-center mx-5 p-2 rounded-full  ml-6 w-[100%] max-w-[200px] "
+                className="mb-4 border-2 flex justify-center items-center p-2 rounded-full  w-[100%] max-w-[200px] "
                 type="email"
                 name="email"
                 placeholder="Enter your Email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />
-            </div>
 
-            <div>
               <input
-                className="mb-4 border-2 flex justify-center items-center mx-5 p-2 rounded-full ml-6  w-[100%] max-w-[200px] px-[auto]"
+                className="mb-4 border-2 flex justify-center items-center  p-2 rounded-full  w-[100%] max-w-[200px] px-[auto]"
                 type="password"
                 placeholder="Password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
               />
             </div>
-            <div className="items-center flex flex-col justify-center ">
+            <div className="items-center gap-3 flex flex-col justify-center ">
               <button className="bg-green-500 px-[70px] py-[8px] rounded-full">
                 Submit
               </button>
