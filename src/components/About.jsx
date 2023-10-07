f9e539ac1a5701d07b4080ae6e93e281785e9435
samuel <olaoluwasamuel119@gmail.com>
2023-10-07 03:24:22 -0600

    hhh

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 const About = () => {
+  const myPic =
+    'https://res.cloudinary.com/dutvrzezk/image/upload/v1696668922/Mikkk_eghovi.jpg';
   const aboutimg =
     'https://res.cloudinary.com/dutvrzezk/image/upload/v1696376468/ttt_qvgktz.jpg';
   return (
@@ -29,17 +31,32 @@ const About = () => {
               />
             }
           </div>
-          <div className="flex flex-col items-center justify-center">
+          <div className="flex flex-col items-center justify-center gap-8">
             <p className="text-4xl  text-black font-bold inline border-b-4 border-black">
               About
             </p>
+            {
+              <img
+                className="w-[250px]"
+                src={myPic}
+                alt="imagee"
+              />
+            }
+            <p className="text-black px-[69px]">
+              I am Michael Robert, the CEO of influxsolarinc. With a profound
+              dedication to clean energy, I've devoted my career to advancing
+              sustainable solutions to different companies across the world.
+              This hands-on experience has shaped my commitment to providing
+              cutting-edge solar solutions that prioritize quality and
+              sustainability.
+            </p>
 
             <p className="text-black mt-[40px] px-5">
-              At SolarEco Solutions, we're passionate about harnessing the power
-              of the sun to create a sustainable and cleaner future for
-              generations to come. Established in [Year], our company is
-              dedicated to providing top-quality solar energy solutions to
-              homes, businesses, and communities across [Location].
+              At influxsolarinc, we're passionate about harnessing the power of
+              the sun to create a sustainable and cleaner future for generations
+              to come.our company is dedicated to providing top-quality solar
+              energy solutions to homes, businesses, and communities across the
+              world.
             </p>
           </div>
         </div>
