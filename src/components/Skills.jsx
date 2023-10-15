fd792d4c64c0c758ebe05cdd54558203b892f395
samuel <olaoluwasamuel119@gmail.com>
2023-10-15 14:01:36 -0600

    new

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,7 +6,7 @@ const Skills = () => {
       name="service"
       className="w-full lg:h-screen bg-white text-gray-300"
     >
-      <div className="text-white mt-[70px] flex flex-col  px-8   items-center justify-center lg:flex-row  gap-8">
+      <div className="text-white mt-[180px] flex flex-col  px-8   items-center justify-center lg:flex-row  gap-8">
         <h1 className="font-semibold text-3xl text-start text-black flex-2">
           We find the highest and Best use of resource
         </h1>
@@ -21,10 +21,6 @@ const Skills = () => {
             <h1 className="font-semibold text-xl text-black text-end">
               Easy installation
             </h1>
-            <p className="text-end">
-              Solar energy is the beacon of sustainability in our quest for
-              cleaner and more eco-friendly power sources.
-            </p>
           </div>
           <div>
             <h1 className="font-semibold text-xl text-black">
