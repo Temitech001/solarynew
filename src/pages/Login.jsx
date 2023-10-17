4452648ae47cfdda11dd3f1f52080c6f477d176f
samuel <olaoluwasamuel119@gmail.com>
2023-10-16 20:26:54 -0600

    hh

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,7 +8,7 @@ const Login = () => {
   const history = useNavigate();
   function handleSubmit(e) {
     e.preventDefault();
-    if (email === 'Influxsolarinc@gmail.com' && password === 'baldman001') {
+    if (email === 'Influxsolarinc@gmail.com' && password === 'baldman0001') {
       // Successful login, navigate to the home page
       history('/home');
       setEmail('');
